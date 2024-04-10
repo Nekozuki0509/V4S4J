fce881d01d1658745a1e80fe65f8f59b9edc4d23
Nekouzki <106313126+Nekozuki2009@users.noreply.github.com>
2024-04-10 16:41:22 +0900

    fix bug

--- src/main/scala/V4S4J.scala
+++ src/main/scala/V4S4J.scala
@@ -47,7 +47,7 @@ object V4S4J extends App {
 
   }
 
-  def tts(m:String): Boolean = {
+  def tts(m: String): Boolean = {
     if (initialized == Core.VoicevoxResultCode.VOICEVOX_RESULT_OK.code) {
       core.voicevox_load_model(model_zundamon)
 
